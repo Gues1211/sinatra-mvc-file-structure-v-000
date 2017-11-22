570423f7d1c62fa427b6616d7b289d9075eee3bc
Guesly  Germain <germain1211@outlook.com>
2017-11-22 16:55:50 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,13 +1,3 @@
 class Model
 
-  class Dog
-    attr_accessor :name, :type, :age
-
-    def initialize(name, type, age)
-      @name = name
-      @type = type
-      @age = age
-    end
-  end
-
 end
