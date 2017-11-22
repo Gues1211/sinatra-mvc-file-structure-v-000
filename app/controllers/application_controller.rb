6891ac1174a92f518fd44d3f90cc44b866e1a482
Guesly  Germain <germain1211@outlook.com>
2017-11-22 16:55:41 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,14 +9,4 @@ class ApplicationController < Sinatra::Base
   	erb :index
   end
 
-  class Dog
-    attr_accessor :name, :type, :age
-
-    def initialize(name, type, age)
-      @name = name
-      @type = type
-      @age = age
-    end
-  end
-
 end
